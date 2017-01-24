807349d684bcdf5ef329f4cffdf3ddf70e8199de
Evita <yevita94@hotmail.com>
2017-01-24 17:50:01 -0600

    Add files via upload

---
+++ inclass-5/avoid.js
@@ -0,0 +1,40 @@
+btn = document.getElementById("avoid");
+info = document.getElementById("Congs");
+win_width = window.innerWidth;
+win_height = window.innerHeight;
+
+window.onload = function(){
+
+	btn.addEventListener("mouseover", move);
+	btn.addEventListener("click", click);
+	window.addEventListener("keydown", down);
+	window.addEventListener("keyup", up);
+}
+
+
+function click(){
+	if(btn.value == "Click me!"){
+		btn.value = "Play Again!";
+		info.style.visibility = "visible";
+		btn.removeEventListener("mouseover", move);
+	}
+	else{
+		btn.value = "Click me!";
+		info.style.visibility = "hidden";
+		btn.addEventListener("mouseover", move);
+	}
+}
+
+function move(){
+		btn.style.left = Math.random()* win_width + "px";
+		btn.style.top = Math.random()* win_height + "px";
+		
+}
+function down(){
+	btn.removeEventListener("mouseover", move);
+}
+function up(){
+	if(btn.value == "Click me!"){
+		btn.addEventListener("mouseover", move);
+	}
+}
